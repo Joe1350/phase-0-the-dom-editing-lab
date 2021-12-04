85e18c1b6ace73026b7a95902e613ad3e202bb08
Joseph Martin <josephmartin1350@gmail.com>
2021-12-04 16:19:37 -0700

    commit tree 03a8434c12b4fe9ec4a143bbc8bab77561a0ce22

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("all test were passing upon arriving, and I understand them")
+// all test were passing upon arriving, and I understand them // clearly you don't cause you were supposed to add them programatically // keep making assumptions dipshit you had it right the first time
+
+document.createElement('body')
+const body = document.querySelector('body')
+body.append()
+
+document.createElement('h1')
+const h1 = document.querySelector('h1')
+h1.innerText = 'My HTML adventure'
+h1.append(body)
+
+// is this how you do it though?
